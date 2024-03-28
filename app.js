e70fe9ef7ecd8324109b7e73945063a7ec24b9f6
Ujayaweera <115028605+Ujayaweera@users.noreply.github.com>
2024-03-28 20:38:38 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const blogRoutes = require('./routes/blogRoutes');
+
+// express app
+const app = express();
+
+// connect to mongodb & listen for requests
+const dbURI = "mongodb+srv://node-user:uthum1234@cluster0.75gbgmo.mongodb.net/nodejs-course?retryWrites=true&w=majority";
+
+mongoose.connect(dbURI)
+  .then(result => app.listen(3000))
+  .catch(err => console.log(err));
+
+// register view engine
+app.set('view engine', 'ejs');
+
+// middleware & static files
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan('dev'));
+
+//blog routes
+app.use('/blogs', blogRoutes);
+
+//routes
+app.get('/', (req, res) => {
+  res.redirect('/blogs');
+});
+
+app.get('/about', (req, res) => {
+  res.render('about', { title: 'About' });
+});
+
+// 404 page
+app.use((req, res) => {
+  res.status(404).render('404', { title: '404' });
+});
